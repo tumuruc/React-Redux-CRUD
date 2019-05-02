43d85dc772575062b734d10c89975d2a8e875408
tumuruc <tumuruc.alex@yahoo.com>
2019-05-02 18:37:48 +0300

    React/redux CRUD app

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+import reducer from "../reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+  reducer,
+  composeWithDevTools());
+
+export default store;
